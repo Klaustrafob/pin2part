f24a2614d9135e0861d57623008c95df19768f8e
Yuriy VG <yuravg@gmail.com>
2020-01-29 11:53:58 +0300

    Run main package from GUI
    Define the debug package in the App package.

--- capPin2Part/capPin2PartApp.tcl
+++ capPin2Part/capPin2PartApp.tcl
@@ -1,19 +1,26 @@
 package require Tcl 8.4
 package require DboTclWriteBasic 16.3.0
 
+package require debug_log
+package require Pin2Part
+
 package provide capPin2PartApp 1.0
 
 namespace eval ::capPin2PartApp {
 }
+
 
 proc ::capPin2PartApp::addDesignAccessoryMenu {} {
     AddAccessoryMenu "Pin2Part" "Launch" "::capPin2PartApp::GUI"
 }
 RegisterAction "_cdnCapTclAddDesignCustomMenu" "::capPin2PartApp::capTrue" "" "::capPin2PartApp::addDesignAccessoryMenu" ""
 
+
 proc exi {} {
     wm withdraw .
 }
+
+debug_log::set_fname "pin2part.log"
 
 proc openPinFile {} {
 
@@ -23,8 +30,11 @@ proc openPinFile {} {
     }
 
     set label "QUARTUS PIN file:"
-    set file [tk_getOpenFile -filetypes $types_quartus -parent .]
-    if [file exists $file] {
+    set fname [tk_getOpenFile -filetypes $types_quartus -parent .]
+    if [file exists $fname] {
+        debug_log::write "Selected Quartus pin-file: $fname"
+        ::Pin2Part::ReadPinFile $fname
+        ::Pin2Part::Draw
     }
 }
 
@@ -40,7 +50,7 @@ proc ::capPin2PartApp::GUI {pLib} {
 
     label .l1 -text "Export Altera pin-file to Cadence Capture part"
 
-    button .open_pin -text "Open Quartus pin-file" -command openPinFile -width 40
+    button .open_pin -text "Open Quartus pin-file, run" -command openPinFile -width 40
 
     button .exi -text EXIT -command exi -width 40
 
