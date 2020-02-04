bb6a280a855a0acc95aa259d31b00a48d78665a0
Yuriy VG <yuravg@gmail.com>
2020-02-04 15:37:13 +0300

    Edit application package
     Improvement:
     - Now Tk application can set path to Quartus pin-file and Cadence
     reference
     - remove unnessesary package launch
     - overwrite GUI.

--- capPin2Part/capPin2PartApp.tcl
+++ capPin2Part/capPin2PartApp.tcl
@@ -1,5 +1,3 @@
-package require Tcl 8.4
-package require DboTclWriteBasic 16.3.0
 
 package require debug_log
 package require Pin2Part
@@ -10,7 +8,6 @@ package provide capPin2PartApp 1.0
 namespace eval ::capPin2PartApp {
     set fname      ""
     set reference  ""
-    set return_str ""
 }
 
 
@@ -29,6 +26,7 @@ debug_log::set_fname "pin2part.log"
 
 
 proc ::capPin2PartApp::openPinFile {} {
+    global label_pinfile
     set types_quartus {
         {"Quartus pin file" {.pin}}
         {"All files"        {*}}
@@ -39,21 +37,21 @@ proc ::capPin2PartApp::openPinFile {} {
         debug_log::write "Selected Quartus pin-file: $fname"
         set ::capPin2PartApp::fname $fname
         Pin2PartCfg::SetFName $fname
+        set label_pinfile $fname
     }
 }
 
 
 proc ::capPin2PartApp::Run {} {
+    global label_status
     set fname $::capPin2PartApp::fname
-    set return_str "ERROR!"
     if [file exists $fname] {
         ::Pin2Part::ReadPinFile $::capPin2PartApp::fname
-        ::Pin2Part::Draw
-        set return_str "Done"
+        ::Pin2Part::Draw $::capPin2PartApp::reference
+        set label_status "Done"
     } else {
-        set return_str "ERROR! Can't find file: $fname"
+        set label_status "ERROR! Can't find file: $fname"
     }
-    set ::capPin2PartApp::return_str $return_str
 }
 
 
@@ -70,6 +68,22 @@ proc ::capPin2PartApp::Init {} {
 }
 
 
+proc ::capPin2PartApp::SetReference {ref} {
+    set ::capPin2PartApp::reference $ref
+    Pin2PartCfg::SetReference $ref
+    puts "*:set ref: $ref"
+    puts $ref
+}
+
+
+proc ::capPin2PartApp::SetRefButton {} {
+    set ref [.eref_des get]
+    Pin2PartCfg::SetReference $ref
+    .eref_des delete 0 end
+    .eref_des insert 0 $ref
+}
+
+
 proc ::capPin2PartApp::GUI {pLib} {
 
     ::capPin2PartApp::Init
@@ -77,31 +91,46 @@ proc ::capPin2PartApp::GUI {pLib} {
     package require Tk 8.4
     wm deiconify .
     wm title . "Pin to Part"
+    wm geometry . 400x300
 
     wm protocol . WM_DELETE_WINDOW {
     }
     set fname $::capPin2PartApp::fname
     set ref   $::capPin2PartApp::reference
 
-    label .l1 -text "Export Altera pin-file to Cadence Capture part\n"
-    label .l2 -text "Current configuration:"
-    label .l3 -text "  pin-file:  $fname"
-    label .l4 -text "  reference: $ref \n"
+    label .ltitle -text "Export Altera pin-file to Cadence Capture part\n"
 
-    button .open_pin -text "Select Quartus pin-file" -command ::capPin2PartApp::openPinFile -width 40
-    button .run_pin -text "RUN" -command ::capPin2PartApp::Run -width 40
+    label .lfile -text "Path to pin-file:"
+    label .lpin_file -textvariable label_pinfile -text $fname
+    button .sel_pin_file -text "Select pin-file" -command ::capPin2PartApp::openPinFile -width 15
 
-    label .l10 -text "$::capPin2PartApp::return_str"
-    button .exi -text "EXIT" -command ::capPin2PartApp::exi -width 40
+    set entry_ref $ref
+    entry .eref_des -textvariable entry_ref -width 10
+    .eref_des insert 0 $ref
+    button .set_ref -text "Set reference" -command "::capPin2PartApp::SetRefButton" -width 15
 
-    grid .l1 -row 0
-    grid .l2 -row 1
-    grid .l3 -row 2
-    grid .l4 -row 3
-    grid .open_pin -row 8
+    button .run_pin -text "RUN" -command ::capPin2PartApp::Run -width 15
+
+    label .lstatus -text idle -textvariable label_status
+    button .exi -text "EXIT" -command ::capPin2PartApp::exi -width 15
+
+    grid .ltitle -row 0
+
+    grid .lfile -row 1 -sticky w
+    grid .lpin_file -row 2 -sticky w
+    grid .sel_pin_file -row 4
+    grid [label .lempty1 -text ""] -row 5
+
+    grid .eref_des -row 6
+    grid .set_ref -row 7
+    grid [label .lempty2 -text ""] -row 8
+
     grid .run_pin -row 9
-    grid .l10 -row 10
-    grid .exi -row 11
+    grid [label .lempty3 -text ""] -row 10
+
+    grid .lstatus -row 11
+    grid .exi -row 12
+
 }
 
 proc ::capPin2PartApp::capTrue {} {
